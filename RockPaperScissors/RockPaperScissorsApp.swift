1ddb2a1fe19077b5e5eb9fd94c579a8119703248
Vittorio Sassu <v.sassu48@gmail.com>
2023-12-22 18:11:43 +0100

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Vittorio Sassu on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
